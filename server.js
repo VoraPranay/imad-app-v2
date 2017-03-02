19a696596841a132bfb9eefc96b265685d921bc8
VoraPranay <ppvora131@gmail.com>
2017-03-03 00:20:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,6 +24,17 @@ app.get('/ui/style.default.css', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '.css'));
+});app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 
 
 app.get('/ui/madi.png', function (req, res) {
