60eb1fdda4524c993b7b62b7ac0cff6bef3c4cc2
VoraPranay <ppvora131@gmail.com>
2017-03-03 00:01:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
+app.get('/ui/pranayindex.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'pranayindex.html'));
 });
 
